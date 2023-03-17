d4e10ead8e61362a827925276654573ba41a915f
RedCometVictory <acemiranda13@att.net>
2023-03-16 22:54:21 -0700

    made main navigation to be fully responsive in regards to height and width, additionally made hover effect for chevron

--- components/nav/MainNav.js
+++ components/nav/MainNav.js
@@ -3,30 +3,13 @@ import Link from 'next/link';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
-import { FaPlus, FaAngleRight, FaChevronRight } from 'react-icons/fa';
+import { FaPlus, FaChevronRight } from 'react-icons/fa';
 import { AiOutlineProject } from 'react-icons/ai';
 import { ImTicket, ImExit, ImEnter } from 'react-icons/im';
-
-// const MobileNav = ({openMenu, setOpenMenu}) => {
-//   return (
-//     <div className="nav__mobile">
-//       <div className="mobile-btn">
-        
-//       </div>
-//       <div className="mobile-btn hidden">
-//         <FaChevronRight />
-//       </div>
-//       <div className="mobile-btn hidden">
-//         <FaChevronRight />
-//       </div>
-//     </div>
-//   )
-// };
 import { MdDashboard } from 'react-icons/md';
 import { logout } from '@/redux/features/auth/authSlice';
 import ModeButton from '@/components/ModeButton';
 import Logo from '../UI/Logo';
-// import ThemePicker from '@/components/theme/ThemePicker';
 
 // TODO:
 // Get role from redux state
@@ -37,14 +20,8 @@ const MainNav = ({openMenu, setOpenMenu}) => {
   const dispatch = useDispatch();
   const { isAuthenticated, user, loading: authLoading } = useSelector(state => state.auth);
   const [hasMounted, setHasMounted] = useState(false);
-  const status = {
-    roleOne: 'Admin',
-    roleTwo: 'Project Manager',
-    roleThree: 'Developer'
-  }
-  let admin = status.roleOne;
-  // let admin = status.roleThree;
-  let role = "Admin";
+
+  const isAdmin = "Admin";
 
   useEffect(() => {
     setHasMounted(true);
@@ -64,85 +41,6 @@ const MainNav = ({openMenu, setOpenMenu}) => {
       toast.error("Failed to logout.")
     }
   };
-
-  // ###### ORIGINAL ######
-  // ###### ORIGINAL ######
-  // ###### ORIGINAL ######
-  // const adminLinks = (
-  //   <li className="nav__link-item">
-  //     <div className="nav__link-group">
-  //       <div className="nav__link-icon">
-  //         <Link passHref href="/new-project">
-  //           <FaPlus />
-  //         </Link>
-  //       </div>
-  //       <Link passHref href="/new-project">
-  //         <span className='nav__link'>New Project</span>
-  //       </Link>
-  //     </div>
-  //   </li>
-  // );
-  
-  // const authLinks = (<>
-  //   <li className="nav__link-item">
-  //     <div className="nav__link-group">
-  //       <div className="nav__link-icon">
-  //         <Link
-  //           passHref
-  //           href={"/"}
-  //         >
-  //           <MdDashboard />
-  //         </Link>
-  //       </div>
-  //       <Link
-  //         passHref
-  //         href={"/"}
-  //       >
-  //         <a className='nav__link'>Dashboard</a>
-  //       </Link>
-  //     </div>
-  //   </li>
-  //   <li className="nav__link-item">
-  //     <div className="nav__link-group">
-  //       <div className="nav__link-icon">
-  //         <Link
-  //           passHref
-  //           href={`/my/projects`}
-  //         >
-  //           <AiOutlineProject />
-  //         </Link>
-  //       </div>
-  //       <Link
-  //         passHref
-  //         href={`/my/projects`}
-  //       >
-  //         <span className='nav__link'>My Projects</span>
-  //       </Link>
-  //     </div>
-  //   </li>
-  //   <li className="nav__link-item">
-  //     <div className="nav__link-group">
-  //       <div className="nav__link-icon">
-  //         <Link
-  //           passHref
-  //           href={`/my/tickets`}
-  //         >
-  //           <ImTicket />
-  //         </Link>
-  //       </div>
-  //       <Link
-  //         passHref
-  //         href={`/my/tickets`}
-  //       >
-  //         <span className='nav__link'>My Tickets</span>
-  //       </Link>
-  //     </div>
-  //   </li>
-  // </>);
-  // ###### ORIGINAL ######
-  // ###### ORIGINAL ######
-  // ###### ORIGINAL ######
-
 
   const adminLinks = (
     <li className="nav__link-item">
@@ -302,7 +200,7 @@ const MainNav = ({openMenu, setOpenMenu}) => {
           {/* <Logo /> */}
           <ul className={`nav__links ${openMenu ? "active" : ""}`}>
             {isAuthenticated && authLinks}
-            {isAuthenticated && role === admin && adminLinks}
+            {isAuthenticated && user.role === isAdmin && adminLinks}
             {isAuthenticated ? (
               <li className="nav__link-item">
                 <div className="nav__link-group">
@@ -363,253 +261,5 @@ const MainNav = ({openMenu, setOpenMenu}) => {
       </div>
     </header>
   )
-}
+};
 export default MainNav;
-
-
-
-
-
-
-
-
-
-
-
-/*
-<div className="nav__content-group lower">
-    <nav className='nav__menu'>
-      <Logo />
-      <ul className="nav__links">
-        {isAuthenticated && authLinks}
-        {isAuthenticated && role === admin && adminLinks}
-        {isAuthenticated ? (
-          <li className="nav__link-item">
-            <div className="nav__link-group">
-              <div className="nav__link-icon">
-                <ImExit />
-              </div>
-              <a
-                // passHref
-                // href="/logout"
-                className='nav__link'
-                onClick={() => logoutHandler()}
-              >
-                Sign Out
-              </a>
-            </div>
-          </li>
-        ) : (
-          <li className="nav__link-item">
-            <div className="nav__link-group">
-              <div className="nav__link-icon">
-                <ImEnter />
-              </div>
-              <Link
-                passHref
-                href="/signin"
-                className='nav__link'
-              >
-                Sign In
-              </Link>
-            </div>
-          </li>
-        )}
-      </ul>
-    </nav>
-  </div>
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useEffect, useState } from 'react';
-// import Link from 'next/link';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useRouter } from 'next/router';
-// import { toast } from 'react-toastify';
-// import { FaPlus } from 'react-icons/fa';
-// import { AiOutlineProject } from 'react-icons/ai';
-// import { ImTicket, ImExit, ImEnter } from 'react-icons/im';
-// import { MdDashboard } from 'react-icons/md';
-// import { logout } from '@/redux/features/auth/authSlice';
-// import ModeButton from '@/components/ModeButton';
-// import Logo from '../UI/Logo';
-// // import ThemePicker from '@/components/theme/ThemePicker';
-
-// // TODO:
-// // Get role from redux state
-// // based on role (Admin, Project Manager, Submitter, Developer) hide links.
-// // swap auth links, show pending on logged in or not
-// const MainNav = () => {
-//   const router = useRouter();
-//   const dispatch = useDispatch();
-//   const { isAuthenticated, user, loading: authLoading } = useSelector(state => state.auth);
-//   const [hasMounted, setHasMounted] = useState(false);
-//   const status = {
-//     roleOne: 'Admin',
-//     roleTwo: 'Project Manager',
-//     roleThree: 'Developer'
-//   }
-//   let admin = status.roleOne;
-//   // let admin = status.roleThree;
-//   let role = "Admin";
-
-//   useEffect(() => {
-//     setHasMounted(true);
-//   }, []);
-  
-//   if (!hasMounted) {
-//     return null;
-//   }
-
-//   const logoutHandler = async () => {
-//     try {
-//       dispatch(logout());
-//       toast.success("Logged out.");
-//       router.push('/');
-//     } catch (err) {
-//       console.error(err);
-//       toast.error("Failed to logout.")
-//     }
-//   };
-
-//   const adminLinks = (
-//     <li className="nav__link-item">
-//       <div className="nav__link-group">
-//         <div className="nav__link-icon">
-//           <FaPlus />
-//         </div>
-//         <Link passHref href="/new-project" className='nav__link'>
-//           New Project
-//         </Link>
-//       </div>
-//     </li>
-//   );
-  
-//   const authLinks = (<>
-//     <li className="nav__link-item">
-//       <div className="nav__link-group">
-//         <div className="nav__link-icon">
-//           <MdDashboard />
-//         </div>
-//         <Link
-//           passHref
-//           href={"/"}
-//           className='nav__link'
-//         >
-//           <a>Dashboard</a>
-//         </Link>
-//       </div>
-//     </li>
-//     <li className="nav__link-item">
-//       <div className="nav__link-group">
-//         <div className="nav__link-icon">
-//           <AiOutlineProject />
-//         </div>
-//         <Link
-//           passHref
-//           href={`/my/projects`}
-//           className='nav__link'
-//         >
-//           My Projects
-//         </Link>
-//       </div>
-//     </li>
-//     <li className="nav__link-item">
-//       <div className="nav__link-group">
-//         <div className="nav__link-icon">
-//           <ImTicket />
-//         </div>
-//         <Link
-//           passHref
-//           href={`/my/tickets`}
-//           className='nav__link'
-//         >
-//           My Tickets
-//         </Link>
-//       </div>
-//     </li>
-//   </>);
-
-//   return (
-//     <header className="nav">
-//       <ModeButton />
-//       {user !== null && (
-//         <div className="nav__user">
-//           <span>Hello, {user?.username}</span> 
-//           <span>As: {user?.role}</span>
-//         </div>
-//       )}
-
-//       {authLoading && (
-//         <div className="">Loading auth info...</div>
-//       )}
-
-//       <nav className='nav__menu'>
-//         <Logo />
-//         <ul className="nav__links">
-//           {isAuthenticated && authLinks}
-//           {isAuthenticated && role === admin && adminLinks}
-//           {isAuthenticated ? (
-//             <li className="nav__link-item">
-//               <div className="nav__link-group">
-//                 <div className="nav__link-icon">
-//                   <ImExit />
-//                 </div>
-//                 <a
-//                   // passHref
-//                   // href="/logout"
-//                   className='nav__link'
-//                   onClick={() => logoutHandler()}
-//                 >
-//                   Sign Out
-//                 </a>
-//               </div>
-//             </li>
-//           ) : (
-//             <li className="nav__link-item">
-//               <div className="nav__link-group">
-//                 <div className="nav__link-icon">
-//                   <ImEnter />
-//                 </div>
-//                 <Link
-//                   passHref
-//                   href="/signin"
-//                   className='nav__link'
-//                 >
-//                   Sign In
-//                 </Link>
-//               </div>
-//             </li>
-//           )}
-//         </ul>
-//       </nav>
-//     </header>
-//   )
-// }
-// export default MainNav;
